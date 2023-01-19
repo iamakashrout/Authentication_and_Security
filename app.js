bd6eb3365ef5f2faf6efa2180c0999ba848ca39d
Akash Rout <116869015+iamakashrout@users.noreply.github.com>
2023-01-19 12:06:25 +0530

    Level 2
    Encryption of password in database and use of environment variables

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -12,10 +14,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email: String,
     password: String
-}
+})
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User=mongoose.model("User", userSchema);
 
