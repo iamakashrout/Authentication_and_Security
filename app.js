afd419dbc3227fb96eb7d56361a7cfc439af764f
Akash Rout <116869015+iamakashrout@users.noreply.github.com>
2023-01-20 12:28:58 +0530

    Final
    User submit feature enabled

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema=new mongoose.Schema({
     email: String,
     password: String,
-    googleID: String
+    googleID: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,8 +89,21 @@ app.get("/login", function(req, res){
 })
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    })
+})
+
+app.get("/submit", function(req, res){
     if(req.isAuthenticated){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
@@ -134,6 +148,23 @@ app.post("/login", function(req, res){
             passport.authenticate("local")(req, res, function(){
                 res.redirect("/secrets");
             })
+        }
+    })
+})
+
+app.post("/submit", function(req, res){
+    const submittedSecret=req.body.secret;
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
         }
     })
 })
